a7af7042483736dadb5216b3a6aa92d373dd5f82
ksenjavassiljeva <ksenja.vassiljeva@ivkhk.ee>
2026-03-05 13:53:58 +0200

    update

--- folder/js/server.js
+++ folder/js/server.js
@@ -1,13 +1,13 @@
-const exspress = require("exspress");
-const path = require('path');
-const app = exspress();
+const express = require("express");
+const path = require("path");
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
-
 const NIMI = process.env.MY_NAME || "Tundmatu nimi (Viga!)";
 
-app.get ('/api/info', (req, res) =>{
-    res.status(200).json ({
+app.get('/api/info', (req, res) => {
+    res.status(200).json({
         misioon: "Iseseisev deplomine edukas",
         meeskond: NIMI,
         aeg: new Date().toISOString()
@@ -18,7 +18,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, '0.0.0.0 ', ()=>{
-    console.log(`API Server töötab selle pordi peale:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`API Server töötab selle pordi peale: ${PORT}`);
 });
-
