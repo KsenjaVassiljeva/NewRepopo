7f70d536ce88143e4b3127a31977d3961f7f20e3
ksenjavassiljeva <ksenja.vassiljeva@ivkhk.ee>
2026-03-05 13:24:35 +0200

    give html and css and update

--- folder/js/server.js
+++ folder/js/server.js
@@ -1,11 +1,12 @@
 const exspress = require("exspress");
+const path = require('path');
 const app = exspress();
 
 const PORT = process.env.PORT || 3000;
 
 const NIMI = process.env.MY_NAME || "Tundmatu nimi (Viga!)";
 
-app.get ('/api/index.html', (req, res) =>{
+app.get ('/api/info', (req, res) =>{
     res.status(200).json ({
         misioon: "Iseseisev deplomine edukas",
         meeskond: NIMI,
@@ -13,14 +14,11 @@ app.get ('/api/index.html', (req, res) =>{
     });
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, '0.0.0.0 ', ()=>{
     console.log(`API Server töötab selle pordi peale:${PORT}`);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const loginButton = document.getElementById('loginButton');
-
-    loginButton.addEventListener('click', function() {
-        alert('Login button clicked!');
-    });
-});
